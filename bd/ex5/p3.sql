689e4f4dc183a143e63eb7ef5fb2ec6bbeb6f609
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-03-18 18:37:45 +0100

    Add: p3.sql

---
+++ bd/ex5/p3.sql
@@ -0,0 +1,13 @@
+/* NORMALIZACIÓN */
+
+/* Realiza el diseño físico de:
+
+Matricula (dni, nombres, apellidos, asignatura). Recuerda que tiene que estar normalizado.*/
+
+CREATE DATABASE matriculas;
+
+CREATE TABLE estudiantes(dni VARCHAR(9) NOT NULL PRIMARY KEY, nombre VARCHAR(30) NOT NULL, apellidos VARCHAR(30) NOT NULL);
+
+CREATE TABLE asignatura(id SERIAL PRIMARY KEY, nombre_asignatura VARCHAR(30) NOT NULL);
+
+CREATE TABLE matricula(dni_estudiante VARCHAR(9) REFERENCES estudiantes(dni), id_asignatura INTEGER REFERENCES asignatura(id));
