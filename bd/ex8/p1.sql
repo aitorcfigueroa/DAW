f0dcd339adeb62b30237b8cf31bc16e385da2022
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-06-03 16:08:21 +0200

    Inicio examen 8

---
+++ bd/ex8/p1.sql
@@ -0,0 +1,16 @@
+--Crea una base de datos en postgres que se llame "bd_frutas_json". Yo ya la tengo creada la BD en mi sistema.
+
+--Todas las tareas van en un archivo sql (p1.sql) que puedo importar a mi sistema gestor (vía i o psql o copiar/pegar el sql).
+
+--1. Le añades una tabla que se llame "frutas" con un campo que sea clave primaria y otro que permita usar JSON, que guardará información sobre frutas.
+
+--2. Una fruta tiene varias propiedades:
+  --Un nombre: naranja, pera, manzana, etc
+  --Un diámetro (en cm): 5, 10, 15, etc
+  --Un origen (ciudad): Vigo, Ourense, Santiago, etc
+
+--3. Añade al menos 5 frutas a tu base de datos
+
+--4. Ponme la consulta para mostrar las frutas que vienen de Ourense
+
+--5. Muéstrame las frutas que comiencen por la letra N
